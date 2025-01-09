c3e6108daaac204bbe326578bff185115d50a56b
egorpopov676767 <e.e.popov@urfu.me>
2025-01-09 16:04:17 +0500

    .

--- calculator_cli/main.cpp
+++ calculator_cli/main.cpp
@@ -7,6 +7,7 @@ using std::cout;
 using std::endl;
 
 int main(const int argc, char **argv) {
+
     if (argc > 1) {
         const auto result = CommandExecutor::tryExecute(argc - 1, &argv[1]);
         cout << result << endl;
